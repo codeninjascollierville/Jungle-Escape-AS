0e372c3581132425e9e00a3dabed348835ba23be
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-07-26 17:45:35 -0500

    on prove your self

--- Jungle Escape AS/Assets/Scripts/Jump.cs
+++ Jungle Escape AS/Assets/Scripts/Jump.cs
@@ -1,36 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Jump : MonoBehaviour
 {
     Rigidbody rigidbody;
 
     float jumpForce = 5.7f;
+    float fallMultiplier = 1.5f; 
 
-    
     public bool isGrounded;
-    
-    float fallMultiplier = 1.5f;
-    
+    public float distToGround = 0.15f;
+
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        isGrounded = true;
     }
 
     void Update()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, .15f);
-        Debug.DrawRay(transform.position, Vector3.down * .15f, Color.red);
-       
-        if(Input.GetButtonDown("Jump")&& isGrounded){
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, distToGround);
+        Debug.DrawRay(transform.position, Vector3.down * distToGround, Color.red);
+
+        //jump movement
+        if(Input.GetButtonDown("Jump") && isGrounded){
             rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
-    
-    if(rigidbody.velocity.y < 0){
-      rigidbody.velocity += Physics.gravity * fallMultiplier * Time.deltaTime;
+        
+        //increase gravity when falling
+        if (rigidbody.velocity.y < 0){
+            rigidbody.velocity += Physics.gravity * fallMultiplier * Time.deltaTime;
+        }
     }
-
-    
-}
 }
